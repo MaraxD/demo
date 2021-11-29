cdd7749e1e2a3dc641b4ac364a25f931962e975d
Mara Olaru <olarumara20@stud.ase.ro>
2021-11-29 23:59:58 +0200

    am adaugat footer+design la butoane+centrare tabel

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './navbar.scss'
+
+
+function Navbar() {
+
+   
+    return (
+        <div className="Navbar">
+            <div className="leftSide">
+                <div>Microsoft Learn Student Ambassador</div>
+                <img src="/images/logo.png" alt=""/>
+            </div>
+            
+            
+            
+        </div>
+    );
+}
+
+export default Navbar
---
+++ src/Tabel/Tabel.js
@@ -0,0 +1,57 @@
+import React,{useState} from 'react'
+import './tabel.scss'
+import data from './data.json'
+
+function Tabel() {
+    
+    const[members, setMembers]=useState(data);
+
+    return (
+        <div className="tabel-container">
+
+                <div className="links"> 
+                    <a href="tech">Tech</a>
+                    <a href="promo">Promovare</a>
+                    <a href="rep">Reprezentare</a>
+                    <a href="hr">HR</a>
+                </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nume</th>
+                        <th>Prenume</th>
+                        <th>Numar perioade active</th>
+                        <th>Numar de telefon</th>
+                        <th>Adresa de mail institutionala</th>
+                        <th>Platforme</th>
+                    </tr>
+                </thead> 
+
+                <tbody>
+                    {members.map((member)=>
+                        <tr>
+                            <td>{member.nume}</td>
+                            <td>{member.prenume}</td>
+                            <td>{member.activitate}</td>
+                            <td>{member.nr_tel}</td>
+                            <td>{member.mail}</td>
+                        </tr>
+                    
+                    )}
+                    
+                </tbody>
+            </table>
+            
+            <div className="save-button">
+                <button> save changes</button>
+            </div>
+            
+            <div className="excel-button">
+                <button> export as excel</button>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Tabel
